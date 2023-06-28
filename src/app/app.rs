d221971639fbc5f59b09bc915f79a419df89c3b3
Tim Holmes-Mitra <tim.holmes-mitra@canonical.com>
2023-06-28 14:29:17 +0100

    chore: tidy app build_and_run fn
    Signed-off-by: Tim Holmes-Mitra <tim.holmes-mitra@canonical.com>

--- src/app/app.rs
+++ src/app/app.rs
@@ -4,20 +4,18 @@ use tonic::transport::Server;
 use tower::ServiceBuilder;
 use tracing::info;
 
-use super::{infrastructure::Infrastructure, interfaces::build_reflection_service};
+use super::infrastructure::Infrastructure;
 
-use super::interfaces::{build_private_servers, build_public_servers};
+use super::interfaces::{build_private_servers, build_public_servers, build_reflection_service};
 use crate::utils;
 
 pub async fn build_and_run() {
-    let socket: SocketAddr = utils::env::get_socket().parse().unwrap();
-
-    let infra = Infrastructure::new().await;
-    let infra = Arc::new(infra);
-
     let layer = ServiceBuilder::new()
         .timeout(Duration::from_secs(30))
         .into_inner();
+
+    let infra = Infrastructure::new().await;
+    let infra = Arc::new(infra);
 
     let server = Server::builder()
         .layer(layer)
@@ -26,6 +24,7 @@ pub async fn build_and_run() {
     let server = build_public_servers(server, infra.clone());
     let server = build_private_servers(server, infra.clone());
 
-    info!("Binding to {}", socket);
+    let socket: SocketAddr = utils::env::get_socket().parse().unwrap();
+    info!("Binding to {socket}");
     server.serve(socket).await.unwrap();
 }
