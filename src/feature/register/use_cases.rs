5c93eedbfbaa9827aa718eac3dba4d2d8acd4cd8
Tim Holmes-Mitra <tim@holmie.xyz>
2023-07-03 20:00:41 +0100

    feat: move register repository calls into infrastructure mod

--- src/feature/register/use_cases.rs
+++ src/feature/register/use_cases.rs
@@ -1,9 +1,9 @@
-use super::errors::RegisterError;
+use super::{errors::RegisterError, infrastructure::create_user_in_db};
 use crate::app::infrastructure::Infrastructure;
 use rand::{distributions::Alphanumeric, Rng};
 
-type Token = String;
-type UserId = str;
+pub type Token = String;
+pub type UserId = str;
 
 #[tracing::instrument]
 pub async fn create_user(uid: &UserId, infra: &Infrastructure) -> Result<Token, RegisterError> {
@@ -15,32 +15,6 @@ pub async fn create_user(uid: &UserId, infra: &Infrastructure) -> Result<Token, 
 
     let token = create_token(&uid);
     create_user_in_db(token, infra).await
-}
-
-async fn create_user_in_db(token: Token, infra: &Infrastructure) -> Result<Token, RegisterError> {
-    let mut pool = match infra.postgres.acquire().await {
-        Ok(p) => p,
-        Err(_) => return Err(RegisterError::FailedToCreateUserRecord),
-    };
-
-    sqlx::query("INSERT INTO users (token) VALUES ($1)")
-        .bind(&token)
-        .execute(&mut pool)
-        .await
-        .map_err(|err| {
-            tracing::error!("Failed to insert user into the db: {err:?}");
-            RegisterError::FailedToCreateUserRecord
-        })
-        .and_then(|pg_result| {
-            let rows_affected = pg_result.rows_affected();
-
-            if rows_affected == 1 {
-                Ok(token)
-            } else {
-                tracing::error!("user insert changed {rows_affected} row(s) but 1 expected");
-                Err(RegisterError::FailedToCreateUserRecord)
-            }
-        })
 }
 
 pub const EXPECTED_UID_LENGTH: usize = 64;
