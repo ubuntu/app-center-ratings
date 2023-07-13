9ca02aab89bc066739652d43a01f8371b8df926f
Tim Holmes-Mitra <tim.holmes-mitra@canonical.com>
2023-07-13 09:09:19 +0100

    feat: jwt expiry to from 30 to 1 days
    Shorter lived JWTs are best practice because compromised tokens
    expire quickly. Although this isn't a mission critical system and the
     'credentials' used by the app store at this stage aren't as
     sensitive as say a username and password, I cannot think of a
     drawback of 1 day expires at this stage.
    
    Signed-off-by: Tim Holmes-Mitra <tim.holmes-mitra@canonical.com>

--- src/utils/jwt.rs
+++ src/utils/jwt.rs
@@ -8,7 +8,7 @@ use tracing::error;
 
 use crate::utils::env;
 
-pub type Token = String;
+static JWT_EXPIRY_IN_DAYS: i64 = 1;
 
 #[derive(Error, Debug)]
 pub enum JwtError {
@@ -18,7 +18,6 @@ pub enum JwtError {
     Unknown,
 }
 
-
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Claims {
     pub sub: String,
@@ -27,13 +26,10 @@ pub struct Claims {
 
 impl Claims {
     pub fn new(sub: String) -> Self {
-        let exp = OffsetDateTime::now_utc().add(Duration::days(30));
+        let exp = OffsetDateTime::now_utc().add(Duration::days(JWT_EXPIRY_IN_DAYS));
         let exp = exp.unix_timestamp() as usize;
 
-        Self {
-            sub,
-            exp,
-        }
+        Self { sub, exp }
     }
 }
 
@@ -46,8 +42,10 @@ impl Jwt {
     pub fn new() -> Self {
         let secret = env::get_jwt_secret();
         let secret = secret.as_str();
-        let encoding_key = EncodingKey::from_base64_secret(secret).expect("failed to load jwt secret");
-        let decoding_key = DecodingKey::from_base64_secret(secret).expect("failed to load jwt secret");
+        let encoding_key =
+            EncodingKey::from_base64_secret(secret).expect("failed to load jwt secret");
+        let decoding_key =
+            DecodingKey::from_base64_secret(secret).expect("failed to load jwt secret");
 
         Self {
             encoding_key,
@@ -58,11 +56,10 @@ impl Jwt {
     pub fn encode(&self, sub: String) -> Result<String, JwtError> {
         let claims = Claims::new(sub);
 
-        jsonwebtoken::encode(&Header::default(), &claims, &self.encoding_key)
-            .map_err(|e| {
-                error!("{e:?}");
-                JwtError::Unknown
-            })
+        jsonwebtoken::encode(&Header::default(), &claims, &self.encoding_key).map_err(|e| {
+            error!("{e:?}");
+            JwtError::Unknown
+        })
     }
 
     pub fn decode(&self, token: &str) -> Result<Claims, JwtError> {
