5dff060918029ce489e3d2c5217fdf4b2fd2e61a
Tim Holmes-Mitra <tim.holmes-mitra@canonical.com>
2023-07-14 13:18:18 +0100

    feat: log new and close span events
    This means when an method is entered it will be logged and when it is
     exited it will be logged (with some timing information). This does
     not log suspend/resume calls for async methods as that becomes too
     verbose.
    
    Signed-off-by: Tim Holmes-Mitra <tim.holmes-mitra@canonical.com>

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     utils::env::init();
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
+        .with_span_events(tracing_subscriber::fmt::format::FmtSpan::CLOSE)
         .init();
     tracing::info!("Starting Ubuntu App Rating Service");
     utils::env::print_env_if_dev();
