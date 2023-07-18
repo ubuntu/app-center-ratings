f4847dafd91fb80a5e84ee4abe58ccf07b3a5a49
Tim Holmes-Mitra <tim.holmes-mitra@canonical.com>
2023-07-18 16:58:54 +0100

    feat: restructure db scripts
    1. `sql/init.sql` sets up the database, tables, users, indexes, etc. It
    should only be run on once.
    2. `sql/teardown.sql` drops everything. It should only be run in dev.
    3. `sql/migrations` is a placeholder for future schema migrations.
    
    Signed-off-by: Tim Holmes-Mitra <tim.holmes-mitra@canonical.com>

--- sql/init.sql
+++ sql/init.sql
@@ -1,18 +1,24 @@
-# Provision the db
+--
+-- Create database and then execute the second set of commands when connected
+-- to the ratings database
+--
 
-```psql
-CREATE DATABASE ratings;
+-- Stage 1
+
+--CREATE DATABASE ratings;
+
+-- Stage 2
 
 CREATE TABLE users (
    id SERIAL PRIMARY KEY,
    client_hash CHAR(64) NOT NULL UNIQUE, -- sha256($MACHINE_ID$USER)
-   created TIMESTAMP NOT NULL DEFAULT NOW(),
-   last_seen TIMESTAMP NOT NULL
+   created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+   last_seen TIMESTAMPTZ NOT NULL
 );
 
 CREATE TABLE votes (
    id SERIAL PRIMARY KEY,
-   created TIMESTAMP NOT NULL DEFAULT NOW(),
+   created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
    user_id_fk INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
    snap_id CHAR(32) NOT NULL,
    snap_revision INT NOT NULL CHECK (snap_revision > 0),
@@ -26,10 +32,9 @@ CREATE TABLE votes (
 CREATE UNIQUE INDEX idx_votes_unique_user_snap ON votes (user_id_fk, snap_id, snap_revision);
 
 -- Permissions
-CREATE USER service WITH PASSWORD $1;
+CREATE USER service WITH PASSWORD 'covfefe!1';
 GRANT ALL PRIVILEGES ON TABLE users TO service;
 GRANT USAGE, SELECT ON SEQUENCE users_id_seq TO service;
 GRANT ALL PRIVILEGES ON TABLE votes TO service;
 GRANT USAGE, SELECT ON SEQUENCE votes_id_seq TO service;
 GRANT CONNECT ON DATABASE ratings TO service;
-```
