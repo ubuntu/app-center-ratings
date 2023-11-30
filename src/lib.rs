e3df1850df048e7d6f63975bf15d53bb457a0752
Zoe Spellman <zoe.spellman@canonical.com>
2023-11-30 10:45:38 -0800

    add bits to manifest

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+//! The `ratings` library represents the code necessary for the Ubuntu app center's
+//! ratings backend.
+
+#![deny(rustdoc::broken_intra_doc_links)]
+#![warn(missing_docs)]
+#![warn(clippy::missing_docs_in_private_items)]
+
 pub mod app;
 pub mod features;
 pub mod utils;
