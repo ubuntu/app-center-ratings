91cb6cbb29950ff63652ac13495fb7d3eecdb42c
Zoe Spellman <zoe.spellman@canonical.com>
2024-01-08 23:40:41 -0800

    Add top-level comment for app mod

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,3 +1,5 @@
+//! Contains all definitions of the main application interface part of the program.
+
 pub use context::{AppContext, RequestContext};
 pub use run::run;
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,10 @@
 //! The `ratings` library represents the code necessary for the Ubuntu app center's
 //! ratings backend.
 
-/* Normally we'd use #! for these attributes, but it breaks the protobuf files and I can't find
-a good way to allow the warnings from `build.rs` */
+#![deny(rustdoc::broken_intra_doc_links)]
+#![warn(missing_docs)]
+#![warn(clippy::missing_docs_in_private_items)]
 
-#[deny(rustdoc::broken_intra_doc_links)]
-#[warn(missing_docs)]
-#[warn(clippy::missing_docs_in_private_items)]
 pub mod app;
-
-#[deny(rustdoc::broken_intra_doc_links)]
-#[warn(missing_docs)]
-#[warn(clippy::missing_docs_in_private_items)]
 pub mod features;
-
-#[deny(rustdoc::broken_intra_doc_links)]
-#[warn(missing_docs)]
-#[warn(clippy::missing_docs_in_private_items)]
 pub mod utils;
