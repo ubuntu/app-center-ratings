d4d57df23736a3bf9329bd4da1f99fa9ddbb08a2
Zoe Spellman <zoe.spellman@canonical.com>
2024-01-08 23:42:21 -0800

    Add basic docs to protobuf generated modules

--- src/features/pb.rs
+++ src/features/pb.rs
@@ -5,17 +5,25 @@
 #![allow(clippy::missing_docs_in_private_items)]
 
 pub mod app {
+    //! Contains protobufs relating to the app features
+
     include!("../../proto/ratings.features.app.rs");
 }
 
 pub mod common {
+    //! Contains common protobufs
+
     include!("../../proto/ratings.features.common.rs");
 }
 
 pub mod user {
+    //! Contains user protobufs
+
     include!("../../proto/ratings.features.user.rs");
 }
 
 pub mod chart {
+    //! Contains chart protobufs
+
     include!("../../proto/ratings.features.chart.rs");
 }
