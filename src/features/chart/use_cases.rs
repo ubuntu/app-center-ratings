c9a2bf11e60ee2372ba36249cd3c7a14dde30ed0
Zoe Spellman <zoe.spellman@canonical.com>
2024-02-15 00:40:38 -0800

    feat: allow top charts by category

--- src/features/chart/use_cases.rs
+++ src/features/chart/use_cases.rs
@@ -3,10 +3,8 @@
 use crate::{
     app::AppContext,
     features::{
-        chart::{
-            entities::Chart, errors::ChartError, infrastructure::get_votes_summary_by_timeframe,
-        },
-        pb::chart::Timeframe,
+        chart::{entities::Chart, errors::ChartError, infrastructure::get_votes_summary},
+        pb::chart::{Category, Timeframe},
     },
 };
 use tracing::error;
@@ -14,8 +12,12 @@ use tracing::error;
 /// Gets a chart over the given [`Timeframe`] within the given [`AppContext`]. Either ends up returning
 /// a [`Chart`] or else one of the many [`ChartError`]s in case the timeframe is invalid or another database error
 /// happens.
-pub async fn get_chart(app_ctx: &AppContext, timeframe: Timeframe) -> Result<Chart, ChartError> {
-    let votes = get_votes_summary_by_timeframe(app_ctx, timeframe)
+pub async fn get_chart(
+    app_ctx: &AppContext,
+    timeframe: Timeframe,
+    category: Option<Category>,
+) -> Result<Chart, ChartError> {
+    let votes = get_votes_summary(app_ctx, timeframe, category)
         .await
         .map_err(|error| {
             error!("{error:?}");
--- src/features/mod.rs
+++ src/features/mod.rs
@@ -1,8 +1,8 @@
 //! Contains various feature implementations for the ratings backend.
 
 pub mod chart;
+pub mod pb;
 pub mod rating;
 pub mod user;
 
 mod common;
-mod pb;
