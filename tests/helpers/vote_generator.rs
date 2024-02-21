182257dae5302fb68fe6b8c39958cdd19fe51aab
Zoe Spellman <zoe.spellman@canonical.com>
2024-02-21 01:31:21 -0800

    refactor: better error handling for joins

--- tests/helpers/vote_generator.rs
+++ tests/helpers/vote_generator.rs
@@ -4,6 +4,22 @@ use super::client::*;
 use crate::helpers;
 use futures::future::join_all;
 use ratings::features::pb::user::{AuthenticateResponse, VoteRequest};
+use thiserror::Error;
+use tonic::Status;
+
+#[derive(Clone, Debug, Error)]
+pub enum GenerateVoteError {
+    #[error("there was a panic while attempting to authenticate the votes: {0}")]
+    Panic(String),
+    #[error("there was a negative response from the server: {0}")]
+    Status(#[from] Status),
+}
+
+impl From<String> for GenerateVoteError {
+    fn from(value: String) -> Self {
+        Self::Panic(value)
+    }
+}
 
 pub async fn generate_votes(
     snap_id: &str,
@@ -11,7 +27,7 @@ pub async fn generate_votes(
     vote_up: bool,
     count: u64,
     client: &TestClient,
-) -> Result<(), Box<dyn std::error::Error>> {
+) -> Result<(), GenerateVoteError> {
     let mut joins = Vec::with_capacity(count as usize);
 
     let snap_id = Arc::new(snap_id.to_string());
@@ -25,11 +41,16 @@ pub async fn generate_votes(
     }
 
     for join in join_all(joins).await {
-        // The second ? returns an error about type sizing for some reason
-        #[allow(clippy::question_mark)]
-        if let Err(err) = join? {
-            return Err(err);
-        }
+        join.map_err(|e| {
+            if e.is_panic() {
+                e.into_panic()
+                    .downcast::<&'static str>()
+                    .unwrap()
+                    .to_string()
+            } else {
+                format!("other error: {}", e)
+            }
+        })??
     }
 
     Ok(())
@@ -40,7 +61,7 @@ async fn register_and_vote(
     snap_revision: i32,
     vote_up: bool,
     client: &TestClient,
-) -> Result<(), Box<dyn std::error::Error + Send>> {
+) -> Result<(), Status> {
     let id: String = helpers::data_faker::rnd_sha_256();
     let response: AuthenticateResponse = client
         .authenticate(&id)
