41a80d3cffa66c1f33f9338c0cf0dadb0f48c344
Zoe Spellman <zoe.spellman@canonical.com>
2024-02-20 18:13:22 -0800

    test: migrate chart tests

--- tests/helpers/vote_generator.rs
+++ tests/helpers/vote_generator.rs
@@ -1,5 +1,8 @@
+use std::sync::Arc;
+
 use super::client_user::*;
 use crate::helpers;
+use futures::future::join_all;
 use ratings::features::pb::user::{AuthenticateResponse, VoteRequest};
 
 pub async fn generate_votes(
@@ -9,9 +12,26 @@ pub async fn generate_votes(
     count: u64,
     client: &UserClient,
 ) -> Result<(), Box<dyn std::error::Error>> {
+    let mut joins = Vec::with_capacity(count as usize);
+
+    let snap_id = Arc::new(snap_id.to_string());
+    let client = Arc::new(client.clone());
     for _ in 0..count {
-        register_and_vote(snap_id, snap_revision, vote_up, client).await?;
+        let snap_id = snap_id.clone();
+        let client = client.clone();
+        joins.push(tokio::spawn(async move {
+            register_and_vote(&snap_id, snap_revision, vote_up, &client).await
+        }));
     }
+
+    for join in join_all(joins).await {
+        // The second ? returns an error about type sizing for some reason
+        #[allow(clippy::question_mark)]
+        if let Err(err) = join? {
+            return Err(err);
+        }
+    }
+
     Ok(())
 }
 
@@ -20,7 +40,7 @@ async fn register_and_vote(
     snap_revision: i32,
     vote_up: bool,
     client: &UserClient,
-) -> Result<(), Box<dyn std::error::Error>> {
+) -> Result<(), Box<dyn std::error::Error + Send>> {
     let id: String = helpers::data_faker::rnd_sha_256();
     let response: AuthenticateResponse = client
         .authenticate(&id)
