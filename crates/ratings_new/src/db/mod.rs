5e223ef8a8366ca62b2e663da9b22d9e8659a150
Dennis Loose <dennis.loose@canonical.com>
2024-09-30 12:14:47 +0200

    test: test User + Vote roundtrip

--- crates/ratings_new/src/db/mod.rs
+++ crates/ratings_new/src/db/mod.rs
@@ -72,3 +72,89 @@ macro_rules! conn {
         &mut *($crate::db::get_pool().await?.acquire().await?)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use sqlx::types::time::OffsetDateTime;
+    use tracing_subscriber::EnvFilter;
+
+    use crate::conn;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn save_and_read_votes() -> Result<(), Box<dyn std::error::Error>> {
+        let client_hash_1 = "0000000000000000000000000000000000000000000000000000000000000001";
+        let client_hash_2 = "0000000000000000000000000000000000000000000000000000000000000002";
+        let snap_id_1 = "00000000000000000000000000000001";
+        let snap_id_2 = "00000000000000000000000000000002";
+
+        tracing_subscriber::fmt()
+            .with_env_filter(EnvFilter::from_default_env())
+            .init();
+
+        let test_users = [
+            user::User::new(client_hash_1),
+            user::User::new(client_hash_2),
+        ];
+
+        let test_votes = [
+            vote::Vote {
+                client_hash: String::from(client_hash_1),
+                snap_id: String::from(snap_id_1),
+                vote_up: true,
+                timestamp: OffsetDateTime::from_unix_timestamp(123).unwrap(),
+                snap_revision: 1,
+            },
+            vote::Vote {
+                client_hash: String::from(client_hash_2),
+                snap_id: String::from(snap_id_2),
+                vote_up: false,
+                timestamp: OffsetDateTime::from_unix_timestamp(456).unwrap(),
+                snap_revision: 2,
+            },
+        ];
+
+        let connection = conn!();
+
+        for user in test_users.into_iter() {
+            user.create_or_seen(connection).await?;
+        }
+
+        for vote in test_votes.into_iter() {
+            vote.save_to_db(connection).await?;
+        }
+
+        let votes_client_1 = vote::Vote::get_all_by_client_hash_and_snap_id(
+            connection,
+            String::from(snap_id_1),
+            String::from(client_hash_1),
+        )
+        .await
+        .unwrap();
+
+        let votes_client_2 = vote::Vote::get_all_by_client_hash_and_snap_id(
+            connection,
+            String::from(snap_id_2),
+            String::from(client_hash_2),
+        )
+        .await
+        .unwrap();
+
+        assert_eq!(votes_client_1.len(), 1);
+        let first_vote = votes_client_1.first().unwrap();
+        assert_eq!(first_vote.snap_id, snap_id_1);
+        assert_eq!(first_vote.client_hash, client_hash_1);
+        assert_eq!(first_vote.snap_revision, 1);
+        assert!(first_vote.vote_up);
+
+        let second_vote = votes_client_2.first().unwrap();
+        assert_eq!(votes_client_2.len(), 1);
+        assert_eq!(second_vote.snap_id, snap_id_2);
+        assert_eq!(second_vote.client_hash, client_hash_2);
+        assert_eq!(second_vote.snap_revision, 2);
+        assert!(!second_vote.vote_up);
+
+        Ok(())
+    }
+}
