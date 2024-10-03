245d3b47aaf31d8b527afc78a90ff8732c990878
Matthew Hagemann <matt.hagemann@canonical.com>
2024-10-03 15:37:01 +0200

    feat: export proto types in ratings_new

---
+++ crates/ratings_new/src/proto/mod.rs
@@ -0,0 +1,12 @@
+pub mod app {
+    include!("ratings.features.app.rs");
+}
+pub mod user{
+    include!("ratings.features.user.rs");
+}
+pub mod common{
+    include!("ratings.features.common.rs");
+}
+pub mod chart{
+    include!("ratings.features.chart.rs");
+}
