cf799f80158381593bc8b9b05ddd18308a61adbe
Matthew Hagemann <matt.hagemann@canonical.com>
2024-11-26 17:22:41 +0200

    feat: tls termination handled by tonic server

--- src/grpc/mod.rs
+++ src/grpc/mod.rs
@@ -1,6 +1,9 @@
-use crate::{db, jwt::JwtVerifier, middleware::AuthLayer, Context};
-use std::net::SocketAddr;
-use tonic::{transport::Server, Status};
+use crate::{config, db, jwt::JwtVerifier, middleware::AuthLayer, Context};
+use std::{fs::read_to_string, net::SocketAddr};
+use tonic::{
+    transport::{Identity, Server, ServerTlsConfig},
+    Status,
+};
 
 mod app;
 mod charts;
@@ -8,6 +11,7 @@ mod user;
 
 use app::RatingService;
 use charts::ChartService;
+use tracing::warn;
 use user::UserService;
 
 impl From<db::Error> for Status {
@@ -20,7 +24,26 @@ pub async fn run_server(ctx: Context) -> Result<(), Box<dyn std::error::Error>> 
     let verifier = JwtVerifier::from_secret(&ctx.config.jwt_secret)?;
     let addr: SocketAddr = ctx.config.socket().parse()?;
 
-    Server::builder()
+    let cert_path = ctx.config.tls_cert_path.clone();
+    let key_path = ctx.config.tls_key_path.clone();
+
+    let builder = match (cert_path, key_path) {
+        (Some(cert_path), Some(key_path)) => {
+            let cert = read_to_string(cert_path)?;
+            let key = read_to_string(key_path)?;
+            let identity = Identity::from_pem(cert, key);
+            Server::builder().tls_config(ServerTlsConfig::new().identity(identity))?
+        }
+        (Some(_), None) | (None, Some(_)) => {
+            panic!("Both TLS certificate and key must be provided, or neither.");
+        }
+        (None, None) => {
+            warn!("TLS is not configured as the environment variables are not set.");
+            Server::builder()
+        }
+    };
+
+    builder
         .layer(AuthLayer::new(verifier))
         .add_service(RatingService::new_server())
         .add_service(ChartService::new_server())
